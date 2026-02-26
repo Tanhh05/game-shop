800b98923a46e4f9d8dfc5fb7be8a033c395860e
ADMIN <phantuananh181205@gmail.com>
2026-02-26 23:44:50 +0700

    "Update 26-02 v2"

--- game-shop-backend/src/main/java/com/example/gameshopbackend/config/WebConfig.java
+++ game-shop-backend/src/main/java/com/example/gameshopbackend/config/WebConfig.java
@@ -26,10 +26,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")   // ✅ cho phép tất cả
-                .allowedMethods("*")
+                .allowedOrigins(
+                        "https://game-shop-v1.onrender.com",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
