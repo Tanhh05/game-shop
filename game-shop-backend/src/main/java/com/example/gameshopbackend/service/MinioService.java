e2617cf6996218052895348cbb812161e0a4b3a3
ADMIN <phantuananh181205@gmail.com>
2026-02-27 01:19:29 +0700

    "Update 26-02 v4"

--- game-shop-backend/src/main/java/com/example/gameshopbackend/service/MinioService.java
+++ game-shop-backend/src/main/java/com/example/gameshopbackend/service/MinioService.java
@@ -33,6 +33,6 @@ public class MinioService {
                         .build()
         );
 
-        return publicUrl + "/" + bucket + "/" + fileName;
+        return publicUrl + "/" + fileName;
     }
 }
